fd15d7ea3313ea752ca1716ad7bf1ca8785bdf22
umibri <brian.uminga@gmail.com>
2022-10-21 00:14:21 -0400

    set up base template with tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-zinc-900">
+      <h1 className="text-white text-3xl">Test</h1>
+    </div>
+  )
+}
+
+export default Home
